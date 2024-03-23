ac3b581c1d8c5f2fc2cc259f7d5e366fe8260d08
Lel Illumina <dragonbilal938@outlook.com>
2024-03-23 23:41:21 +0500

    add pretty print for ReadFolder

--- ReadFolder.py
+++ ReadFolder.py
@@ -11,11 +11,18 @@ url = "https://nekoweb.org/api/files/readfolder"
 
 pathname = input("Path name to query: ")
 
-querystring = {"pathname":pathname}
+querystring = {"pathname": pathname}
 
 headers = {"Authorization": API_KEY}
 
 response = requests.request("GET", url, headers=headers, params=querystring)
-json = json.dumps(json.loads(response.text), indent=4)
+data = json.loads(response.text)
 
-print (json)
+directories = [item for item in data if item['dir']]
+files = [item for item in data if not item['dir']]
+
+for item in directories:
+    print(f"\033[34m{item['name']}/\033[0m")
+
+for item in files:
+    print(f"\033[32m{item['name']}\033[0m")
